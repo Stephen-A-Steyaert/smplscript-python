8883a5b7bba4f7266f0e3eeb5e67cebca7c38133
Stephen Steyaert <savagery2005@gmail.com>
2025-03-14 16:31:09 -0400

    Added blank main file; constants file
    Added the base main file and the first constants.

---
+++ src/smplscript/constants.py
@@ -0,0 +1,7 @@
+TT_INT          = "TT_INT";
+TT_PLUS         = "TT_PLUS";
+TT_MINUS        = "TT_MINUS";
+TT_MUL          = "TT_MUL";
+TT_DIV          = "TT_DIV";
+TT_LPAREN       = "TT_LPAREN";
+TT_RPAREN       = "TT_RPAREN";
